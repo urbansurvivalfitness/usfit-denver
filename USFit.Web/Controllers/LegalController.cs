648c1ef321816674f3cd87bdc0ca9b26d2aebe7f
cbertolasio <cbertolasio@gmail.com>
2012-04-15 19:44:15 -0600

    added legal files and controller, robots, humans, 404, etc

---
+++ USFit.Web/Controllers/LegalController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace USFit.Web.Controllers
+{
+    public class LegalController : Controller
+    {
+        [ActionName("terms-of-service")]
+        public ActionResult TermsOfService()
+        {
+            return View("TermsOfService");
+        }
+
+        [ActionName("terms-of-use")]
+        public ActionResult TermsOfUse()
+        {
+            return View("TermsOfUse");
+        }
+
+        [ActionName("legal-information")]
+        public ActionResult Legal()
+        {
+            return View("Legal");
+        }
+    }
+}
