f14624134b2a2bd750e03daa087657b1b6d313a7
cbertolasio <cbertolasio@gmail.com>
2012-06-18 23:02:11 -0600

    added paypal buttons, cancel page, and success page.

---
+++ USFit.Web/Controllers/PurchaseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace USFit.Web.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        public ActionResult Cancel()
+        {
+            return View();
+        }
+
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
