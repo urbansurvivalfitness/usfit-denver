0fc2ebb024b6207abc2d247f30c06c2a9702d4eb
cbertolasio <cbertolasio@gmail.com>
2012-08-28 01:59:48 -0600

    fixed pathing to jquery mobile images

--- USFit.Web/App_Start/BundleMobileConfig.cs
+++ USFit.Web/App_Start/BundleMobileConfig.cs
@@ -13,6 +13,8 @@ namespace USFit.Web {
             
             bundles.Add(new StyleBundle("~/Content/jquerymobile/css")
                 .Include("~/Content/jquery.mobile-{version}.css", "~/Content/themes/blue-and-gold/usf-blue-and-orange*"));
+
+            bundles.Add(new StyleBundle("~/Content/jquerymobile/images").Include("~/Content/themes/blue-and-gold/images/*"));
                 
         }
     }
